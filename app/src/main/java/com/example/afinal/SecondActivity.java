a5acb5d383553f9d227617b38b159d05f043a151
Makayla <makaylawms0426@gmail.com>
2022-05-23 21:57:34 -0700

    Have added in the blockClick brian method as well as the delays for all of the moves. Still need to create the opponentMove method.

--- app/src/main/java/com/example/afinal/SecondActivity.java
+++ app/src/main/java/com/example/afinal/SecondActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,8 +13,8 @@ import android.widget.Toast;
 
 import java.lang.Thread;
 
-public class SecondActivity extends AppCompatActivity  {
-
+public class SecondActivity extends AppCompatActivity
+{
     int userHealth = 100;
     boolean userBlocking = false;
     int opponentHealth = 100;
@@ -21,7 +22,8 @@ public class SecondActivity extends AppCompatActivity  {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         ImageView img = (ImageView) findViewById(R.id.userCharacterView);
@@ -34,93 +36,144 @@ public class SecondActivity extends AppCompatActivity  {
         {
             Drawable myDrawable = getResources().getDrawable(R.drawable.character1);
             img.setImageDrawable(myDrawable);
-        }
+        }//end if
         else if(characterNum == 2)
         {
             Drawable myDrawable = getResources().getDrawable(R.drawable.character2);
             img.setImageDrawable(myDrawable);
-        }
+        }//end first else if
         else if(characterNum == 3)
         {
             Drawable myDrawable = getResources().getDrawable(R.drawable.character3);
             img.setImageDrawable(myDrawable);
-        }
+        }//end second else if
         else if(characterNum == 4)
         {
             Drawable myDrawable = getResources().getDrawable(R.drawable.character4);
             img.setImageDrawable(myDrawable);
-        }
+        }//end third else if
 
-    }
+    }//end onCreate
 
     public void attackClick(View v)
     {
         TextView current = (TextView) findViewById(R.id.currentMoveView);
         current.setText("Attack");
 
-        //add delay in
+        //creates object that will allow for the delayed code
+        Handler handler = new Handler();
 
-        int rand = (int) (Math.random() * 100);
-        if(rand <= 40 && opponentBlocking == false)
+        //calls postDelayed method
+        handler.postDelayed(new Runnable()
         {
-            opponentHealth -= 15;
-            TextView opponentView = (TextView)findViewById(R.id.opponentHealthView);
-            opponentView.setText("Opponent Health: " + opponentHealth);
-            current.setText("Your attack did 15 points of damage");
-        }
-        else if(opponentBlocking == true)
-        {
-            current.setText("Your opponent was blocking was not damaged by your attack");
-        }
-        else
-        {
-            current.setText("Your opponent dodged the attack");
-        }
+            //creates a runnable object which is all the code that will run after the time limit is up
+            @Override
+            public void run()
+            {
+                int rand = (int) (Math.random() * 100);
+                if(rand <= 40 && opponentBlocking == false)
+                {
+                    opponentHealth -= 15;
+                    TextView opponentView = (TextView)findViewById(R.id.opponentHealthView);
+                    opponentView.setText("Opponent Health: " + opponentHealth);
+                    current.setText("Your attack did 15 points of damage");
+                }//end if
+                else if(opponentBlocking == true)
+                {
+                    current.setText("Your opponent was blocking was not damaged by your attack");
+                }//end else if
+                else
+                {
+                    current.setText("Your opponent dodged the attack");
+                }//end else
 
-        opponentBlocking = false;
-        //add delay in
-        //add opponent move in
-    }
+                opponentBlocking = false;
+
+                handler.postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        opponentMove();
+                    }//end inner run method
+                }, 5000); //end inner postDelayed method
+
+            }//end outer run method
+            //insert the amount of time delayed
+        }, 5000);//end outer postDelayed method
+
+    }//end attackClick method
 
     public void specialClick(View v)
     {
         TextView current = (TextView) findViewById(R.id.currentMoveView);
         current.setText("Special Attack");
 
-        //add delay in
+        Handler handler = new Handler();
 
-        int rand = (int)(Math.random() * 100);
-        if(rand <= 30 && opponentBlocking == false)
+        handler.postDelayed(new Runnable()
         {
-            opponentHealth -= 25;
-            TextView opponentView = (TextView)findViewById(R.id.opponentHealthView);
-            opponentView.setText("Opponent Health: " + opponentHealth);
-            current.setText("Your special attack did 25 points of damage");
-        }
-        else if(opponentBlocking == true)
-        {
-            opponentHealth -= 10;
-            TextView opponentView = (TextView)findViewById(R.id.opponentHealthView);
-            opponentView.setText("Opponent Health: " + opponentHealth);
-            current.setText("Your opponent was blocking so your special attack did 10 points of damage");
-        }
-        else
-        {
-            current.setText("Your opponent dodged the attack");
-        }
+            @Override
+            public void run()
+            {
+                int rand = (int)(Math.random() * 100);
+                if(rand <= 30 && opponentBlocking == false)
+                {
+                    opponentHealth -= 25;
+                    TextView opponentView = (TextView)findViewById(R.id.opponentHealthView);
+                    opponentView.setText("Opponent Health: " + opponentHealth);
+                    current.setText("Your special attack did 25 points of damage");
+                }//end if
+                else if(opponentBlocking == true)
+                {
+                    opponentHealth -= 10;
+                    TextView opponentView = (TextView)findViewById(R.id.opponentHealthView);
+                    opponentView.setText("Opponent Health: " + opponentHealth);
+                    current.setText("Your opponent was blocking so your special attack did 10 points of damage");
+                }//end else if
+                else
+                {
+                    current.setText("Your opponent dodged the attack");
+                }//end else
 
-        opponentBlocking = false;
-        //add delay in
-        //add opponent move in
-    }
+                opponentBlocking = false;
+
+                handler.postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        opponentMove();
+                    }//end inner run
+                }, 5000); //end inner postDelayed
+
+            }//end outer run
+        }, 5000);//end outer postDelayed
+
+    }//end specialClick
 
     public void blockClick(View v)
     {
-        Toast.makeText(this, "The block button has been clicked", Toast.LENGTH_SHORT).show();
-    }
-}
+        userBlocking = true;
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                opponentMove();
+            }//end run
+        }, 5000); //end postDelayed
 
+    }//end blockClick
+
+    public void opponentMove()
+    {
+        Toast.makeText(this, "opponent's turn", Toast.LENGTH_SHORT).show();
+    }//end opponentMove
+
+}//end secondActivity class
 
 //Code for how to set imageView image from https://docs.microsoft.com/en-us/dotnet/api/android.widget.imageview.setimagedrawable?view=xamarin-android-sdk-12
-//Code for sleep timer from https://www.javatpoint.com/thread-sleep-in-java
+//Code for sleep timer from https://docs.microsoft.com/en-us/dotnet/api/android.os.handler.postdelayed?view=xamarin-android-sdk-12
